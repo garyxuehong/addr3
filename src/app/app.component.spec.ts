6a7d9c9fc37447fb9771ac35eb2df936b1e2f8ff
garyxue <gary.xuehong@gmail.com>
2016-12-06 21:38:50 +1100

    npm

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,14 +1,24 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async } from '@angular/core/testing';
+import { APP_BASE_HREF } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { EmployeeService } from './service';
+import { State } from './data';
 
 describe('AppComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterModule.forRoot([])
+      ],
       declarations: [
         AppComponent
       ],
+      providers: [
+        { provide: APP_BASE_HREF, useValue: '/' },
+        { provide: EmployeeService, useValue: getEmployeeService() }]
     });
   });
 
@@ -18,16 +28,14 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
-
   it('should render title in a h1 tag', async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     let compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(compiled.querySelector('h1').textContent).toContain('Address book');
   }));
 });
+
+function getEmployeeService() {
+  return new EmployeeService(new State());
+}
--- src/app/service/employee.service.spec.ts
+++ src/app/service/employee.service.spec.ts
@@ -1,16 +1,157 @@
 /* tslint:disable:no-unused-variable */
+import 'should';
 
 import { TestBed, async, inject } from '@angular/core/testing';
-import { EmployeeServiceService } from './employee.service';
 
-describe('EmployeeServiceService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [EmployeeServiceService]
-    });
-  });
+import { EmployeeService } from './employee.service';
+import { State, Employee, Department } from '../data';
 
-  it('should ...', inject([EmployeeServiceService], (service: EmployeeServiceService) => {
-    expect(service).toBeTruthy();
-  }));
-});
+const DATA_FAKE_ID = 'fake id';
+const DATA_FAKE_FIRSTNAME = 'fake first name';
+const DATA_FAKE_LASTNAME = 'fake last name';
+
+describe('Service', () => {
+	let state, service;
+	beforeEach(() => {
+		state = new State();
+		service = new EmployeeService(state);
+	})
+	it('should init correctly', () => {
+		service.should.not.be.null();
+		service.should.not.be.undefined();
+	})
+	describe('functionality', () => {
+		let employee;
+		beforeEach(() => {
+			employee = new Employee();
+			employee.id = DATA_FAKE_ID;
+			employee.firstname = DATA_FAKE_FIRSTNAME;
+			employee.lastname = DATA_FAKE_LASTNAME;
+		})
+		describe('of adding', () => {
+			it('should success', () => {
+				const newId = service.addEmployee(employee);
+				newId.should.not.be.null();
+				newId.should.not.be.undefined();
+				newId.should.not.be.equal(DATA_FAKE_ID);
+				service.getState().employees.length.should.be.equal(1);
+				const addedEmployee = service.getState().employees[0];
+				addedEmployee.firstname.should.equal(DATA_FAKE_FIRSTNAME);
+				addedEmployee.lastname.should.equal(DATA_FAKE_LASTNAME);
+			})
+		})
+		describe('of updating', () => {
+			beforeEach(() => {
+				service.addEmployee(employee);
+			})
+			it('should success', () => {
+				const modifyEmployee = new Employee();
+				modifyEmployee.id = employee.id;
+				const newFirstname = 'new firstname';
+				const newLastname = 'new lastname';
+				modifyEmployee.firstname = newFirstname;
+				modifyEmployee.lastname = newLastname;
+				service.updateEmployee(modifyEmployee);
+				employee.firstname.should.be.equal(newFirstname);
+				employee.lastname.should.be.equal(newLastname);
+			})
+		})
+		describe('of deleting', () => {
+			let newId;
+			beforeEach(() => {
+				newId = service.addEmployee(employee);
+			})
+			it('should success', () => {
+				const removed = service.removeEmployeeById(newId);
+				removed.should.not.be.null();
+				removed.should.not.be.undefined();
+				service.getState().employees.length.should.be.equal(0);
+			})
+		})
+		describe('(advance)', () => {
+			let emp1, emp2, emp3;
+			let depart1, depart2;
+			beforeEach(() => {
+				depart1 = new Department({
+					id: 'd1',
+					name: 'Department Super 1'
+				});
+				depart2 = new Department({
+					id: 'd2',
+					name: 'Department Crazy 2'
+				});
+				emp1 = new Employee({
+					firstname: 'fn1',
+					lastname: 'ln1',
+					department: depart1
+				});
+				emp2 = new Employee({
+					firstname: 'fn2',
+					lastname: 'ln2',
+					department: depart1
+				});
+				emp3 = new Employee({
+					firstname: 'fn3',
+					lastname: 'ln3',
+					department: depart2
+				});
+				service.addEmployee(emp1);
+				service.addEmployee(emp2);
+				service.addEmployee(emp3);
+			})
+			describe('of sorting ', () => {
+				describe('by firstname', () => {
+					it('should sort correctly', () => {
+						service.sortEmployee('firstname', true);
+						service.getState().viewEmployees[0].firstname.should.be.equal('fn1');
+						service.getState().viewEmployees[1].firstname.should.be.equal('fn2');
+						service.getState().viewEmployees[2].firstname.should.be.equal('fn3');
+						service.sortEmployee('firstname', false);
+						service.getState().viewEmployees[0].firstname.should.be.equal('fn3');
+						service.getState().viewEmployees[1].firstname.should.be.equal('fn2');
+						service.getState().viewEmployees[2].firstname.should.be.equal('fn1');
+					})
+				})
+				describe('by lastname', () => {
+					it('should sort correctly', () => {
+						service.sortEmployee('lastname', true);
+						service.getState().viewEmployees[0].lastname.should.be.equal('ln1');
+						service.getState().viewEmployees[1].lastname.should.be.equal('ln2');
+						service.getState().viewEmployees[2].lastname.should.be.equal('ln3');
+						service.sortEmployee('lastname', false);
+						service.getState().viewEmployees[0].lastname.should.be.equal('ln3');
+						service.getState().viewEmployees[1].lastname.should.be.equal('ln2');
+						service.getState().viewEmployees[2].lastname.should.be.equal('ln1');
+					})
+				})
+			})
+			describe('of search', () => {
+				describe('by name', () => {
+					it('should filter employees by name correctly', () => {
+						service.searchEmployee({
+							employeeSearchValue: '2'
+						});
+						service.getState().viewEmployees.length.should.be.equal(1);
+						service.getState().viewEmployees[0].firstname.should.be.equal('fn2');
+						service.getState().viewEmployees[0].lastname.should.be.equal('ln2');
+					})
+				})
+				describe('by department', () => {
+					it('should filter employees by departments correctly', () => {
+						service.searchEmployee({
+							departmentSearchValue: 'super'
+						});
+						service.getState().viewEmployees.length.should.be.equal(2);
+						service.getState().viewEmployees[0].firstname.should.be.equal('fn1');
+						service.getState().viewEmployees[1].firstname.should.be.equal('fn2');
+						service.searchEmployee({
+							departmentSearchValue: 'crazy'
+						});
+						service.getState().viewEmployees.length.should.be.equal(1);
+						service.getState().viewEmployees[0].firstname.should.be.equal('fn3');
+					})
+				})
+			})
+		})
+	})
+})
